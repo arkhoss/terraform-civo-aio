3eb96bf0b94e0674da3f81893c4561b74644a2d4
David Caballero <d@dcaballero.net>
2025-10-30 06:07:57 +0000

    feat(iac): updated outputs to have create conditionals

--- outputs.tf
+++ outputs.tf
@@ -1,34 +1,33 @@
 output "network_id" {
-  value       = civo_network.this[0].id
+  value       = var.create_network ? civo_network.this[0].id : {}
   description = "network id"
 }
 
 output "firewall_id" {
-  value       = civo_firewall.this[0].id
+  value       = var.create_firewall ? civo_firewall.this[0].id : {}
   description = "firewall id"
 }
 
 output "cluster_name" {
-  value       = civo_kubernetes_cluster.this[0].name
+  value       = var.create_cluster ? civo_kubernetes_cluster.this[0].name : {}
   description = "The name of the Kubernetes cluster"
 }
 
 output "cluster_api_endpoint" {
-  value       = civo_kubernetes_cluster.this[0].api_endpoint
+  value       = var.create_cluster ? civo_kubernetes_cluster.this[0].api_endpoint : {}
   description = "The API endpoint of the Kubernetes cluster"
 }
 
 output "cluster_kubeconfig" {
-  value       = civo_kubernetes_cluster.this[0].kubeconfig
+  value       = var.create_cluster ? civo_kubernetes_cluster.this[0].kubeconfig : {}
   description = "The KubeConfig for the Kubernetes cluster"
   sensitive   = true
 }
 
 output "reserved_ip" {
-  value       = civo_reserved_ip.this[0].ip
+  value       = var.create_reserved_ip ? civo_reserved_ip.this[0].ip : {}
   description = "A reserved IP address to be used by an ingress controller or similar"
 }
-
 
 output "database_info" {
   description = "Details of all created Civo databases"
