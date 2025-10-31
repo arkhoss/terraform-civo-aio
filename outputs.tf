863ee168aabb65c854624c5a1369bce6506ae89f
David Caballero <d@dcaballero.net>
2025-10-31 03:25:05 +0000

    feat(iac): fixed null return for outputs when conditions are not met

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,31 @@
 output "network_id" {
-  value       = var.create_network ? civo_network.this[0].id : {}
+  value       = var.create_network ? try(civo_network.this[0].id, null) : null
   description = "network id"
 }
 
 output "firewall_id" {
-  value       = var.create_firewall ? civo_firewall.this[0].id : {}
+  value       = var.create_firewall ? try(civo_firewall.this[0].id, null) : null
   description = "firewall id"
 }
 
 output "cluster_name" {
-  value       = var.create_cluster ? civo_kubernetes_cluster.this[0].name : {}
+  value       = var.create_cluster ? try(civo_kubernetes_cluster.this[0].name, null) : null
   description = "The name of the Kubernetes cluster"
 }
 
 output "cluster_api_endpoint" {
-  value       = var.create_cluster ? civo_kubernetes_cluster.this[0].api_endpoint : {}
+  value       = var.create_cluster ? try(civo_kubernetes_cluster.this[0].api_endpoint, null) : null
   description = "The API endpoint of the Kubernetes cluster"
 }
 
 output "cluster_kubeconfig" {
-  value       = var.create_cluster ? civo_kubernetes_cluster.this[0].kubeconfig : {}
+  value       = var.create_cluster ? try(civo_kubernetes_cluster.this[0].kubeconfig, null) : null
   description = "The KubeConfig for the Kubernetes cluster"
   sensitive   = true
 }
 
 output "reserved_ip" {
-  value       = var.create_reserved_ip ? civo_reserved_ip.this[0].ip : {}
+  value       = var.create_reserved_ip ? try(civo_reserved_ip.this[0].ip, null) : null
   description = "A reserved IP address to be used by an ingress controller or similar"
 }
 
